13d8c2c44cc310ad959ef47d79dbb1806b891b37
Isaac Hillaker <izacistan@protonmail.com>
2023-08-30 22:34:04 -0500

    Data validation complete.

--- Module 1/Topic 2/Responsive Web App/FirstResponsiveWebAppHillaker/Models/CalcAgeModel.cs
+++ Module 1/Topic 2/Responsive Web App/FirstResponsiveWebAppHillaker/Models/CalcAgeModel.cs
@@ -1,19 +1,27 @@
-﻿namespace FirstResponsiveWebAppHillaker.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FirstResponsiveWebAppHillaker.Models
 {
     public class CalcAgeModel
     {
-        public string Name { get; set; }
-        public int BirthYear { get; set; }
-        public int Age { get; set; }
+        [Required(ErrorMessage = "You must type in your name!")]
+        [RegularExpression("^[a-zA-Z]*$", ErrorMessage = "Your name can only be letters!.")]
+        public string? Name { get; set; }
 
-        public int CalcAge() 
+        [Required(ErrorMessage = "You must enter your birth year!")]
+        [Range(1900, 2100, ErrorMessage = "Please enter a valid birth year between 1900 and 2100.")]
+        public int? BirthYear { get; set; }
+
+        public int? Age { get; set; }
+
+        public int? CalcAge() 
         {
             // Get the current year
             int currentYear = DateTime.Now.Year;
             // Get the user's birth year
-            int birthYear = BirthYear;
+            int? birthYear = BirthYear;
             // Calculate age
-            int age = currentYear - birthYear;
+            int? age = currentYear - birthYear;
             // If the birthday hasn't occurred yet this year, subtract 1 from the age
             if (DateTime.Now < new DateTime(currentYear, 12, 31))
             {
