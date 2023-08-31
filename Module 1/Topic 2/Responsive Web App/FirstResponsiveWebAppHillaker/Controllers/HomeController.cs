a34bf7480628b9d55062a81677960bfcafd12b21
Isaac Hillaker <izacistan@protonmail.com>
2023-08-30 22:18:35 -0500

    Primary functionality achieved in ResponsiveWebApp assignment.

---
+++ Module 1/Topic 2/Responsive Web App/FirstResponsiveWebAppHillaker/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using FirstResponsiveWebAppHillaker.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FirstResponsiveWebAppHillaker.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(CalcAgeModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                ViewBag.Name = model.Name;
+                ViewBag.Age = model.CalcAge();
+            }
+            else
+            {
+                ViewBag.Age = 0;
+            }
+            return View(model);
+        }
+    }
+}
---
+++ Module 1/Topic 2/Responsive Web App/FirstResponsiveWebAppHillaker/Models/CalcAgeModel.cs
@@ -0,0 +1,25 @@
+﻿namespace FirstResponsiveWebAppHillaker.Models
+{
+    public class CalcAgeModel
+    {
+        public string Name { get; set; }
+        public int BirthYear { get; set; }
+        public int Age { get; set; }
+
+        public int CalcAge() 
+        {
+            // Get the current year
+            int currentYear = DateTime.Now.Year;
+            // Get the user's birth year
+            int birthYear = BirthYear;
+            // Calculate age
+            int age = currentYear - birthYear;
+            // If the birthday hasn't occurred yet this year, subtract 1 from the age
+            if (DateTime.Now < new DateTime(currentYear, 12, 31))
+            {
+                age--;
+            }
+            return age;
+        }
+    }
+}
